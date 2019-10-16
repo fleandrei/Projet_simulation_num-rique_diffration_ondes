9ad12f6cddb3365d8c2cdbebeb321c8889e78330
thomas-knb <thomasknieb@hotmail.fr>
2019-10-16 11:19:08 +0200

    tests matrices

--- 1d.jl
+++ 1d.jl
@@ -3,10 +3,11 @@ using Printf
 ########### VARIABLE #############
 k = 2*pi
 lambda = 2*pi/k # longueur d'onde
-h = lambda/10
+h = lambda/10  #pas de la grille
 alpha = pi # angle de l'onde incidente
 a = 1 # rayon de l'obstacle
 taille_espace=6 # taille total de la grille
+taille_matrice=convert(Int64, taille_espace*1/h)
 
 ########## FONCTION ##############
 
@@ -29,11 +30,23 @@ end
 ########### CODE #############
 
 
+# declaration de la matrice
+M = ones(Float64,taille_matrice, taille_matrice)
 
-M = ones(Float64, 60, 60)
-println(M[30, 34])
+for i = 1:taille_matrice
+	for j=1:taille_matrice
+		x,y=coordonnees(i,j,h,taille_espace)
+		r,lambda=conversion_polaire(x,y)
+		if r<1
+			M[i,j]=0
+		else
+			M[i,j]=1
+		end
+	end
+end
 
-println(conversion_polaire(1, 1))
+#println(M[30, 34])
+#println(conversion_polaire(1, 1))
 
 
 
