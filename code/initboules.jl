ae02c29f752c2197ca7965a6b5fd52e41b24707a
3703599 <fleandrei@yahoo.com>
2020-01-08 02:29:11 +0100

    modification de initboules.jl: Ajout d'une fonction initbouleGrid qui permet de générer des boules alignées et espacées à des intervales constants + Modification de la fontion initbouleAlea

--- code/initboules.jl
+++ code/initboules.jl
@@ -15,11 +15,11 @@ function distance(x1,y1,x2,y2)
 end
 
 
-function initBoules()
+function initBoulesAlea(Np) # Crée des boulles dont le nombre, la position et le rayon sont aléatoires
 	nb_boules_min = 2
 	nb_boules_max = 20
 	nb_boules = rand(nb_boules_min:nb_boules_max)
-	boules = zeros(Float64, nb_boules, 3)
+	boules = zeros(Float64, nb_boules, 4)
 
 	breaking = 0
 
@@ -51,6 +51,7 @@ function initBoules()
 			boules[i+1, 1] = x
 			boules[i+1, 2] = y
 			boules[i+1, 3] = r
+			boules[i+1, 4] = Np
 			i = i+1
 		end
 	end
@@ -59,31 +60,66 @@ function initBoules()
 	return (boules, nb_boules)
 end
 
-(boules, nb_boules) = initBoules()
 
-taille_espace = 10
-h = 10
-breaking = 0
+function initBoulesGrid(NbrBoules, TailleMat, Np) # Crée NbrBoules boulles allignées et espacé à interval constant
+	boules = [[] for i=1:NbrBoules]
+	nbrBoule_cote=floor(Int64, sqrt(NbrBoules))  #Nombre de boules le long de chaque dimenssion de la matrice
+	#nbrBoule_cote=nbrBoule_cote + floor(Int64, (NbrBoules - nbrBoule_cote*nbrBoule_cote + 1)/2) 
 
-taille_matrice = taille_espace * h
-M = zeros(Float64, taille_matrice, taille_matrice)
-
-for i = 1:taille_matrice
-	for j = 1:taille_matrice
-		breaking = 0
-		for k = 1:nb_boules
-			if (distance(i/h, j/h, boules[k,1], boules[k,2]) < boules[k,3] && breaking == 0)
-				breaking = 1
-			end
-		end
-		if (breaking == 0)
-			M[i,j] = 1
-		end
+	if (NbrBoules - nbrBoule_cote*nbrBoule_cote !=0) 
+		nbrBoule_cote=nbrBoule_cote + 1
 	end
+	
+	espace_Boule=TailleMat/nbrBoule_cote    # Longueur nécéssaire pour représenter une boule (la boule en elle même plus son espace vital) sur la matrice.
+	espace_Vital=espace_Boule*0.1   # L'espace vital de chaque boulle correspond à 10% de l'espace_Boule
+	Rayon= (espace_Boule-espace_Vital)/2
+
+	count=0
+	for i in 1:nbrBoule_cote #Parcourt les lignes de la matrice
+		j=0
+		while (count<NbrBoules && j<nbrBoule_cote) #Parcourt les colonnes de la matrice
+			push!(boules[count+1], (i-1)*espace_Boule + espace_Boule/2 - TailleMat/2, j*espace_Boule + espace_Boule/2 - TailleMat/2, Rayon, Np)
+			j=j+1
+			count =count+1
+		end
+
+	end 
+
+	return (boules, count)
+
 end
+
+
+
+
+#######Code exemple
+
+Np=14
+#(boules, nb_boules) = initBoulesAlea(Np)
+#taille_espace = 10
+#(boules, nb_boules)= initBoulesGrid(100, taille_espace, Np )
+#h = 10
+#breaking = 0
+#println(boules)
+#taille_matrice = taille_espace * h
+#M = zeros(Float64, taille_matrice, taille_matrice)
+
+#for i = 1:taille_matrice
+#	for j = 1:taille_matrice
+#		breaking = 0
+#		for k = 1:nb_boules
+#			if (distance(i/h, j/h, boules[k,1], boules[k,2]) < boules[k,3] && breaking == 0)
+#				breaking = 1
+#			end
+#		end
+#		if (breaking == 0)
+#			M[i,j] = 1
+#		end
+#	end
+#end
 
 # Affichage graphique
 	
-imshow(M, extent=(0, 10, 0, 10))
-savefig("multiboules.svg")
+#imshow(M, extent=(0, 10, 0, 10))
+#savefig("multiboules.svg")
 
